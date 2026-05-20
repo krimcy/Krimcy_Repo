26b116b84e822b380fad3ebcd7d5685dcbe967ca
krimcy behrani <krimcybehrani@krimcys-MacBook-Air.local>
2026-05-20 14:00:27 +0200

    Add main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-def numbers(num1, num2):
-    return num1* num2
-    
+"""This is a simple Python program that defines a function to make product of 2 numbers."""
+
+
+def numbers(num1: int, num2: int) -> int:
+    """This function takes two numbers and returns their product. """
+
+    x: int = 10
+    return (num1 * num2) + x
+
+
+print(numbers(4, 7))
